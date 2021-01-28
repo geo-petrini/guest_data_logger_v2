f20ce9729f7f4427ed2911129327c8c6d6f4d8af
stefanognesa <61271502+stefanognesa@users.noreply.github.com>
2021-01-28 13:07:06 +0100

    Create GuestDataLogger.sql

---
+++ src/files/GuestDataLogger.sql
@@ -0,0 +1,37 @@
+DROP SCHEMA IF EXISTS GuestDataLogger;
+CREATE SCHEMA GuestDataLogger;
+USE GuestDataLogger;
+
+CREATE TABLE user(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(25),
+    cognome VARCHAR(25),
+    username VARCHAR(25) NOT NULL,
+    admin TINYINT(1) DEFAULT 0
+);
+
+CREATE TABLE stand(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(25) NOT NULL,
+    luogo VARCHAR(25) NOT NULL,
+    data_inizio TIMESTAMP NOT NULL,
+    data_fine TIMESTAMP NOT NULL
+);
+
+CREATE TABLE webcam(
+    id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE stat(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data TIMESTAMP NOT NULL,
+    numero_persone INT NOT NULL
+);
+
+CREATE TABLE chiave(
+    chiave VARCHAR(48) PRIMARY KEY,
+    stand_id INT NOT NULL,
+    webcam_id INT NOT NULL,
+    FOREIGN KEY(stand_id) REFERENCES stand(id),
+    FOREIGN KEY(webcam_id) REFERENCES webcam(id)
+);
