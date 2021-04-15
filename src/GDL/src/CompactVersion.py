92bda4aa5a6241690479d4f9e13d6f55be3d9f26
samuelagustoni <61267692+samuelagustoni@users.noreply.github.com>
2021-04-15 13:18:25 +0200

    Add files via upload

--- src/GDL/src/CompactVersion.py
+++ src/GDL/src/CompactVersion.py
@@ -19,6 +19,13 @@ import multiprocessing
 from queue import Queue
 import sys
 import keyboard
+import logging
+
+proxies = {
+ 'http': 'http://10.20.4.118:8888',
+ 'https': 'http://10.20.4.118:8888'
+}
+
 
 #il metodo get() della classe Queue, di default ha un parametro block settato a true che blocca il flusso del programma fino a quando la coda si riempie.
 
@@ -31,10 +38,17 @@ class SenderData (thread.Thread):
         self.ex = ex
     
     def run(self):
+        timeN = datetime.datetime.now()
+        timeF = datetime.datetime.now()
+        timeF += timedelta(seconds=10)
         condizione = self.checkEx()
         while condizione:
-            self.send_data()
+            if timeN < timeF:
+                self.send_data()
+                timeF = datetime.datetime.now()
+                timeF += timedelta(seconds=10)
             condizione = self.checkEx()
+        timeF = datetime.datetime.now()
         print("Fuori dal run")
         #sys.exit()
         return
@@ -57,16 +71,18 @@ class SenderData (thread.Thread):
     #@param minutes Minuti attuali.
     #@param secs Secondi attuali.
     def dataToJSON(self):
-        try:
-            v = self.q.get(True, 3)
-            myJson = {
-                "data":v["time"],
-                "num_persone": v["count"],
-                "api_key":self.api_key
-                }
-            return myJson
-        except:
-            print("dataToJSON: exception")
+        #try:
+        v = self.q.get(True, 11)
+        print(v)
+        myJson = {
+            "data":v["time"],
+            "num_persone": v["count"],
+            "api_key":self.api_key
+            }
+        print(myJson)
+        return myJson
+        #except:
+        #    print("dataToJSON: exception")
 
     #-----------------------------------------------------------------------------   
 
@@ -81,20 +97,20 @@ class SenderData (thread.Thread):
     #@param password Password dell'utente da testare.
 
     def send_data(self):
-        #url = 'http://localhost:8080/GuestDataLogger/scripts/insertStat.php'
-        url = 'http://127.0.0.1'
-        req = requests.post(url, json= self.dataToJSON())
-        print(req.text)
+        url = 'http://samtinfo.ch/gdl/scripts/insertStat.php'
+        #url = 'http://127.0.0.1'
+        req = requests.post(url, json= self.dataToJSON(), proxies=proxies)
+        s = req.text
+        print(f'request text; {s}')
 
 #----------------------------------------------------------------------------------------
-
-
 
 class StartWindow():
     
     # Inizializza il frame di login, istanziando tutti gli oggetti utili.
     def __init__(self, q):
         self.q = q
+        self.webcamport = 0
         self._build_frame()
         self.main_frame.protocol("WM_DELETE_WINDOW", self.on_closing)
         self._build_logo()
@@ -102,7 +118,7 @@ class StartWindow():
         self._build_key_input()
         self._build_option_menu()
         self._build_button()
-        self.main_frame.mainloop()        
+        self.main_frame.mainloop()  
     #-------------------------------- Menu Frame in Tkinter ---------------------------------------
 
     #/////////////////// Istanzia il frame /////////////////
@@ -123,11 +139,11 @@ class StartWindow():
     def _build_button(self):
         btn = Button(self.main_frame, text ="Start", width=20,command=self.buttonPressed)
         btn.place(x=60,y=320)
-
+    
     #//////////////// Istanzia l'option menu /////////////////////
     def _build_option_menu(self):
         variable = tk.StringVar(self.main_frame)
-        optionList = self.returnCameraIndexes()
+        optionList = self.returnCameraIndexes() #ti da la lista tutta
         variable.set("Seleziona la webcam")
         opt = tk.OptionMenu(self.main_frame, variable, *optionList)
         opt.config(width=23, font=('Helvetica', 10))
@@ -140,7 +156,9 @@ class StartWindow():
         labelTest.place(x=30,y=220)
         def callback(*args):
             labelTest.configure(text="The selected item is {}".format(variable.get()))
+            self.webcamport = int(variable.get())
         variable.trace("w", callback)
+        
 
     #//////////////// Istanzia e carica il logo /////////////////////
     def _build_logo(self):
@@ -171,15 +189,16 @@ class StartWindow():
 
     #/////////////////// Testa la connessione con il server ////////////////////////
     def testConnection(self, api_key):
-        #url = 'http://127.0.0.1:8080/GuestDataLogger/scripts/checkKey.php'
-        url = 'http://127.0.0.1'
+        
+        url = 'http://samtinfo.ch/gdl/scripts/checkKey.php'
+        #url = 'http://127.0.0.1'
         #custom_header = {"api_key":api_key}
         myJson = {
                 "api_key":api_key
                 }
-        response = requests.post(url, json=myJson)
+        response = requests.post(url, json=myJson, proxies=proxies)
         print(response.status_code)
-        if response.status_code == 404: #200: 
+        if response.status_code == 386: 
             return True
         else:
             return False
@@ -204,7 +223,8 @@ class StartWindow():
         else:
             if self.returnCameraIndexes() != ["None"]:
                 if self.testConnection(api_key) is True:
-                    self.q.put( {'api_key':api_key} )
+                    print("entered")
+                    self.q.put( {'api_key':api_key, 'webcam_port':self.webcamport} )
                     self.main_frame.destroy()
                 else:
                     L4 = Label(self.main_frame, text="API Key invalid, try again.")
@@ -219,21 +239,255 @@ class StartWindow():
     def returnCameraIndexes(self):
         index = 0
         freeIndexes = []
-        while True:
+        while index < 255:
             video = cv.VideoCapture(index, cv.CAP_DSHOW)
-            if not video.isOpened():
-                break
-            else:
+            if video.isOpened():
                 freeIndexes.append(index)
+                index += 1
+            else:
+                index += 1
             video.release()
-            index += 1
         if len(freeIndexes) < 1:
             return ["None"]
         return freeIndexes
 
-class CameraWindow():
+class Capture(thread.Thread):
+
+    def __init__(self, q, frames_queue, camera_port):
+        thread.Thread.__init__(self)
+        self.camera_port = camera_port
+        self.q = q
+        self.frames_queue = frames_queue
+        self.faceProto = "opencv_face_detector.pbtxt"
+        self.faceModel = "opencv_face_detector_uint8.pb"        
+        self.runstate = True
+        # Load network
+        self.faceNet = cv.dnn.readNet(self.faceModel, self.faceProto)
+    
+    def detectFaces(self, cap):
+        frameFace = None
+        count = 0
+        error = None
+        #prende il frame attuale della camera.
+        hasFrame, frame = cap.read()
+        if not hasFrame:    #se non individua frame, esce
+            cv.waitKey()
+        else:
+
+            #ridimensione del frame del 200% con il metodo apposito.
+            try:
+                frame = self.rescale_frame(frame,percent=200)
+            except:
+                logging.exception('error scaling frame')
+                error = True
+
+            #prende il numero di box/cornici create. Se non viene trovata nessuna, continua.
+            frameFace, bboxes = self.getFaceBox(self.faceNet, frame) 
+
+            #conteggio dei volti presenti nel frame.
+            for face in bboxes:
+                count+=1
+            
+        print(f'count current detected faces: {count}')
+        return {'frame':frameFace, 'count':count, 'error':error}
+        
+
+#------------------------------ Scansione volti ------------------------------    
+
+    #Genera  i box/cornici attorno ad ogni volto trovato per frame.
+    #@Param frame Frame attuale.
+    #@Param net Network.
+    def getFaceBox(self, net, frame, conf_threshold=0.7):
+        frameOpencvDnn = frame.copy()
+        frameHeight = frameOpencvDnn.shape[0]
+        frameWidth = frameOpencvDnn.shape[1]
+        blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+        net.setInput(blob)
+        detections = net.forward()
+        bboxes = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3] * frameWidth)
+                y1 = int(detections[0, 0, i, 4] * frameHeight)
+                x2 = int(detections[0, 0, i, 5] * frameWidth)
+                y2 = int(detections[0, 0, i, 6] * frameHeight)
+                bboxes.append([x1, y1, x2, y2])
+                cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (255, 0, 0), int(round(frameHeight/150)), 8)
+        return frameOpencvDnn, bboxes
+
+    #------------------------------Resize del frame-------------------------------
+
+    #esegue un resizing del frame in base alla percentuale passata.
+    #@param frame Frame da ridimensionare.
+    #@param percent Percentuale di ridimensionamento.
+    def rescale_frame(self, frame, percent=75):
+        width = int(frame.shape[1] * percent/ 100)
+        height = int(frame.shape[0] * percent/ 100)
+        dim = (width, height)
+        return cv.resize(frame, dim, interpolation =cv.INTER_AREA)   
+
+    def halt(self):
+        logging.debug('stop thread')
+        self.runstate = False
+
+    def run(self):
+        logging.info("camera start")
+        #cattura dello schermo
+        cap = cv.VideoCapture(self.camera_port, cv.CAP_DSHOW)
+        padding = 20
+        last_face_number = None
+        count = 0
+
+        timeN = datetime.datetime.now() #current time
+        timeF = datetime.datetime.now() + timedelta(seconds=10) #time to send data
+        #cv.waitKey(1) < 0 and
+        while cv.waitKey(1) < 0 and self.runstate:
+
+            #print(self.runstate)
+            face_number = 0 
+            detected = self.detectFaces(cap)
+
+            if detected:
+                #conteggio totale delle persone.
+
+                if last_face_number:
+                    if last_face_number < detected['count']:
+                        last_face_number = detected['count']
+                        count += 1
+                else:
+                    last_face_number = detected['count']
+                    count = detected['count']            
+           
+            if not detected['error']:
+                logging.debug("adding data to frames queue")
+                self.frames_queue.put( detected )
+                #self.update_picture( cv.cvtColor(detected['frame'], cv.COLOR_BGR2RGB) )
+                #self.update_counter(count)              
+            timeN = datetime.datetime.now()
+
+            if timeN >= timeF:
+                #dataToJSON(timeN.strftime('%Y-%m-%d %H:%M:%S'), count, api_key)
+                self.q.put( {'time':timeN.strftime('%Y-%m-%d %H:%M:%S'), 'count':count} )
+                #azzeremento del last_face_number
+                last_face_number = None
+                # #azzeramento count
+                count = 0
+                timeF = datetime.datetime.now() + timedelta(seconds=10)
+
+        
+        #TODO clean close cv
+        cv.destroyAllWindows()
+        self.frames_queue.put('quit')
+        logging.info('end while - exiting webcam procedure')
+        print('end while - exiting webcam procedure')
+
+
+
+class CameraWindowN():
+    def __init__(self, frames_queue, webcam_port=0, ex=None):
+        self.frames_queue = frames_queue
+        self.ex = ex
+        self.runstate = True
+        self.faceProto = "opencv_face_detector.pbtxt"
+        self.faceModel = "opencv_face_detector_uint8.pb"
+        self._build_frame()
+        self._build_label()
+        #self.startFaceRecognition(webcam_port)
+        self.capture = webcam_port
+        self.rth = None
+           
+
+    # Istanzia il frame che conterrà l'immagine della webcam
+    def _build_frame(self):
+        #Definisce nuovo frame.
+        self.top = Tk()
+        #setta le dimensioni del frame.
+        self.top.geometry("1290x700")
+        #setta il background bianco.
+        self.top.configure(background='white')
+        #aggiunge il titolo al frame.
+        self.top.wm_title("Guest Data Logger v2")
+        #setta a non ridimensionabile il frame.        
+        self.top.resizable(width=False, height=False)
+        self.top.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def _build_label(self):
+        self.label = tk.Label(self.top, height=675, width=1290)
+        self.label.pack()
+        self.labelInfo = tk.Label(self.top, text="Conteggio: 0", font=('Helvetica', 12), fg='blue')
+        self.labelInfo.pack()
+        self.label.place(x=0,y=25)
+        self.labelInfo.place(x=0,y=0)
+
+    def update_picture(self, frame):
+        image = Image.fromarray(frame)
+        finalFrame =  ImageTk.PhotoImage(image)           
+        self.label.configure(image = finalFrame)   
+        self.label.image = finalFrame
+
+    def update_counter(self, count):
+        if(count > 1 or count == 0):
+            self.labelInfo.configure(text = "Conteggio: " + str(count) + " persone")
+        else:
+            self.labelInfo.configure(text = "Conteggio: " + str(count) + " persona")
+
+    def on_closing(self):
+        self.ex.put({'exit':True})
+        self.runstate = False
+        logging.info(f'setting runstate = {self.runstate}')
+        print(f'setting runstate = {self.runstate}')
+        
+        self.top.destroy()
+        self.frames_queue.put('quit')
+        
+        #TODO check if self.rth is to be closed somehow
+
+        #while self.rth.is_alive():
+        #    print('rth alive')
+        #    time.sleep(0.1)
+        
+
+    def _refresh(self, data_queue):
+        logging.debug('frame refresh started')
+        while self.runstate:
+            try:
+                data = data_queue.get(False)    #get non blocking
+            except Exception as e:
+                data = None   #capture exception in case of Empty get from queue
+            #logging.debug(f'data_queue value: {data}')
+            #print(f'data_queue value: {data}')
+            if data:
+                if data == 'quit':
+                    print('---------------------------------------------')
+                    self.runstate = False
+                else: 
+                    #logging.debug('refreshing data')
+                    try:
+                        self.update_picture(data['frame'])
+                    except Exception as e:
+                        logging.exception('error updating frame')
+
+                    try:
+                        self.update_counter(data['count'])
+                    except Exception as e:
+                        logging.exception('error updating count')                    
+                
+            #time.sleep(0.01)
+        logging.debug('frame refresh ended')
+        print('frame refresh ended')
+
+    def run(self):
+        logging.debug("starting frames refresh thread")
+        self.rth = thread.Thread(target=self._refresh, args=[self.frames_queue])
+        self.rth.start()
+        logging.debug("starting tk mainloop")
+        self.top.mainloop() 
+
+class CameraWindow(thread.Thread):
     
     def __init__(self, q, webcam_port=0, ex=None):
+        thread.Thread.__init__(self)
         self.q = q
         self.ex = ex
         self.runstate = True
@@ -241,8 +495,10 @@ class CameraWindow():
         self.faceModel = "opencv_face_detector_uint8.pb"
         self._build_frame()
         self._build_label()
-        self.startFaceRecognition(webcam_port)
-        
+        #self.startFaceRecognition(webcam_port)
+        self.capture = webcam_port
+
+
         self.top.mainloop()
     
     # Istanzia il frame che conterrà la webcam
@@ -274,25 +530,10 @@ class CameraWindow():
         self.label.place(x=0,y=25)
         self.labelInfo.place(x=0,y=0)
 
-    #modifica dei parametri in entrata (api key) : guestdatalogger
-    #Permette di iniziare la detection dei volti.
-    #@param host Host del database.
-    #@param user user con cui connettersi.
-    #@param passwd password dell'utente.
-    def startFaceRecognition(self, capture=0):
-       
-        self.threadweb = thread.Thread(target = lambda : self.webcam(capture))
-        self.threadweb.daemon = True
-        #self.threadweb._stop_event = thread.Event()
-        #print(self.threadweb)
-        self.threadweb.start()
-        #if self.threadweb.is_alive():
-        #    print('self.threadweb is alive')
-
     def update_picture(self, frame):
         image = Image.fromarray(frame)
         finalFrame =  ImageTk.PhotoImage(image)           
-        self.label.configure(image = finalFrame)    #TODO usare un canvas
+        self.label.configure(image = finalFrame)   
         self.label.image = finalFrame
 
     def update_counter(self, count):
@@ -301,13 +542,13 @@ class CameraWindow():
         else:
             self.labelInfo.configure(text = "Conteggio: " + str(count) + " persona")
 
-    def webcam(self, capture):
-        
+    def run(self):
+        print("funzia")
         # Load network
         faceNet = cv.dnn.readNet(self.faceModel, self.faceProto)
 
         #cattura dello schermo
-        cap = cv.VideoCapture(capture, cv.CAP_DSHOW)
+        cap = cv.VideoCapture(self.capture, cv.CAP_DSHOW)
         padding = 20
         last_face_number = None
         count = 0
@@ -436,42 +677,44 @@ class CameraWindow():
 
     #-----------------------------------------------------------------------------
     #metodo per aggiungere il json : guestdatalogger
-
-class CameraRunner(thread.Thread):
-    def __init__(self,  funct):
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = thread.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()    
-
     
 
 if __name__ == '__main__':
     ex = Queue()
     ex.put({'exit':False})  # TODO covertire da coda ad evento
-    q =  Queue()
-    sw = StartWindow(q) #si chiude da sola
+    sender_queue =  Queue()
+    '''
+    sw = StartWindow(sender_queue) #si chiude da sola
     
-    sw_return_val = q.get()
+    sw_return_val = sender_queue.get()
+    print(sw_return_val)
     api_key = sw_return_val["api_key"]
-    #webcam_port = sw_return_val["webcam_port"]
+    webcam_port = sw_return_val["webcam_port"]
     
-    sd = SenderData(q, api_key, ex)
+    sd = SenderData(sender_queue, api_key, ex)
     sd.daemon = True
     sd.start()
+    '''
 
-    cw = CameraWindow(q, webcam_port=0, ex=ex)
+    frames_queue = Queue()
+    #cw = CameraWindow(sender_queue, webcam_port, ex=ex)
+    cap = Capture(sender_queue, frames_queue, 0)
+    cap.start()
 
-    sd.join()
+    cw = CameraWindowN(frames_queue, ex=ex)
+    cw.run()    #code stops here as tk mainloop is started
 
+    cap.runstate = False
+    cw = None
+    #cw.start()
+
+    #sd.join()
+    '''
     print("Thread morte")
-    while cw.threadweb.is_alive():
+    while cw.is_alive():
         time.sleep(0.5)  
-        print(f" sd alive: {sd.is_alive()},cw alive: {cw.threadweb.is_alive()}")  
+        print(f" sd alive: {sd.is_alive()},cw alive: {cw.is_alive()}")  
     # NOTA: IDLE cattura tutte le eccezioni, compresa la SystemExit.
     # Provare senza IDLE.
+    '''
     sys.exit(0)
