985ea1088392ff431ecedbc9c3b549e5300b310d
stefanognesa <61271502+stefanognesa@users.noreply.github.com>
2021-05-06 15:31:12 +0200

    Delete old site

--- src/localDataSite/Configuration/adminUser.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //First argument username, second argument password. You must to have the access to database ScanSpect and to create table.
-    return ["adminUser", "!Ciao123"];
-?>
--- src/localDataSite/Configuration/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //Set the route of the site. At the end you must add '/';
-    return "http://localhost:8080/localDataSite";
-?>
